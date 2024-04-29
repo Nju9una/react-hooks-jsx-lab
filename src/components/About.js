8150e2e21ab01a5b043d80ce5fd25c40f50cf0e6
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-04-29 05:55:32 +0300

    hooks jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "/home/studentmoringa/phase2/react-hooks-jsx-lab/src/data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Insert your content here</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
+
